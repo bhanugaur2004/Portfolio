04afd5738ac684517ba6c6ebe7a150d43edd131e
Bhanu <bhanugaur2004@gmail.com>
2023-07-23 11:01:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop = 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+//             });
+//         };
+//     });
+// };
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin: 'bottomm'});
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Python Developer', 'Java Developer', 'App Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
